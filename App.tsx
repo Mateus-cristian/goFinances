3a59eeca40d4b9b4736c57df2aa758cec77df963
mateus-cristian <mateus_cristian@outlook.com>
2022-10-06 06:28:34 -0300

    feat: configure app

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import Dashboard from './src/screens/Dashboard';
+import { ThemeProvider } from 'styled-components'
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+import theme from './src/global/styles/theme';
+import { Text, View } from 'react-native';
+import React from 'react';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+
+  if (!fontsLoaded) {
+    return <View><Text>Carregando</Text></View>
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <View style={{ backgroundColor: '#eee', flex: 1 }}>
+        <Dashboard />
+      </View>
+    </ThemeProvider >
+  );
+}
+
+
