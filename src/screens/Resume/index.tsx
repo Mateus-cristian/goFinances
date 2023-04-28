ddbf5e627e818a2c67dd770702c40856dfa87fbf
mateus-cristian <mateus_cristian@outlook.com>
2023-04-27 22:07:45 -0300

    feat: Summary of the entire period

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import HistoryCard from '../../components/HistoryCard'
 import { Container, LoadContainer, MonthSelect, Month, MonthSelectButton, MonthSelectIcon, Header, Title, Content, ChartContainer } from './styles'
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import { categories } from '../../utils/categories';
 import { VictoryPie } from 'victory-native'
 import { RFValue } from 'react-native-responsive-fontsize';
@@ -35,8 +34,12 @@ interface CategoryData {
 
 export default function Resume() {
     const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([])
+    const [totalByCategoriesAllPeriod, setTotalByCategoriesAllPeriod] = useState<CategoryData[]>([])
     const [selectedDate, setSelectedDate] = useState(new Date())
     const [isLoading, setIsLoading] = useState(true)
+
+    const [onlyAllPeriod, setOnlyAllPeriod] = useState(false);
+
     const theme = useTheme()
 
     const { user } = useAuth();
@@ -70,12 +73,21 @@ export default function Resume() {
         const responseFormatted: TransactionProps[] = returnDataFirebase;
 
         const totalByCategory: CategoryData[] = [];
-        const expensives = responseFormatted.filter((expensive) =>
-            expensive.type === 'negative' &&
-            new Date(expensive.date).getMonth() === selectedDate.getMonth() &&
-            new Date(expensive.date).getFullYear() === selectedDate.getFullYear()
+        let expensives: any[] = [];
+        if (!onlyAllPeriod) {
+            expensives = responseFormatted.filter((expensive) =>
+                expensive.type === 'negative' &&
+                new Date(expensive.date).getMonth() === selectedDate.getMonth() &&
+                new Date(expensive.date).getFullYear() === selectedDate.getFullYear()
+            )
+        } else {
+            expensives = responseFormatted.filter((expensive) => expensive.type === 'negative')
+        }
 
-        )
+        console.log(expensives);
+        console.log(selectedDate);
+        console.log(onlyAllPeriod)
+
 
         const expensiveTotal = expensives
             .reduce((acumullator: number, expensive: TransactionProps) => {
@@ -113,11 +125,16 @@ export default function Resume() {
         setIsLoading(false)
     }
 
+    // Apenas troca o período do filtro
+    const handlePeriod = () => {
+        setOnlyAllPeriod(!onlyAllPeriod);
+    }
+
 
 
     useFocusEffect(useCallback(() => {
         loadData()
-    }, [selectedDate]))
+    }, [selectedDate, onlyAllPeriod]))
 
     return (
         <Container>
@@ -132,49 +149,81 @@ export default function Resume() {
                 }}
             >
                 <MonthSelect>
-                    <MonthSelectButton onPress={() => handleDateChange('prev')}>
+                    <MonthSelectButton onPress={() => handlePeriod()}>
                         <MonthSelectIcon name='chevron-left' />
                     </MonthSelectButton>
 
                     <Month>
-                        {format(selectedDate, 'MMMM, yyyy', { locale: ptBR })}
+                        {onlyAllPeriod ? 'Todo o periodo' : 'Por mês'}
                     </Month>
 
-                    <MonthSelectButton onPress={() => handleDateChange('next')}>
+                    <MonthSelectButton onPress={() => handlePeriod()}>
                         <MonthSelectIcon name='chevron-right' />
                     </MonthSelectButton>
                 </MonthSelect>
+                {!onlyAllPeriod && (
+                    <MonthSelect>
+                        <MonthSelectButton onPress={() => handleDateChange('prev')}>
+                            <MonthSelectIcon name='chevron-left' />
+                        </MonthSelectButton>
+
+                        <Month>
+                            {format(selectedDate, 'MMMM, yyyy', { locale: ptBR })}
+                        </Month>
+
+                        <MonthSelectButton onPress={() => handleDateChange('next')}>
+                            <MonthSelectIcon name='chevron-right' />
+                        </MonthSelectButton>
+                    </MonthSelect>
+                )}
                 {
                     isLoading ?
                         <LoadContainer>
                             <ActivityIndicator color={theme.colors.secondary} size='large' />
                         </LoadContainer> :
                         <>
-                            <ChartContainer>
-                                <VictoryPie data={totalByCategories}
-                                    colorScale={totalByCategories.map(category => category.color)}
-                                    x='percent'
-                                    y='total'
-                                    labelRadius={80}
-                                    style={{
-                                        labels: {
-                                            fontSize: RFValue(18),
-                                            fontWeight: 'bold',
-                                            fill: '#fff'
-                                        }
-                                    }}
-                                />
-                            </ChartContainer>
-
-                            {
-                                totalByCategories.map(item => (
-                                    <HistoryCard
-                                        key={item.key}
-                                        title={item.name}
-                                        amount={item.totalFormatted}
-                                        color={item.color}
+                            {!onlyAllPeriod ? (
+                                <ChartContainer>
+                                    <VictoryPie data={totalByCategories}
+                                        colorScale={totalByCategories.map(category => category.color)}
+                                        x='percent'
+                                        y='total'
+                                        labelRadius={80}
+                                        style={{
+                                            labels: {
+                                                fontSize: RFValue(18),
+                                                fontWeight: 'bold',
+                                                fill: '#fff'
+                                            }
+                                        }}
                                     />
-                                ))
+                                </ChartContainer>
+                            ) : (
+                                <ChartContainer>
+                                    <VictoryPie data={totalByCategories}
+                                        colorScale={totalByCategories.map(category => category.color)}
+                                        x='percent'
+                                        y='total'
+                                        labelRadius={80}
+                                        style={{
+                                            labels: {
+                                                fontSize: RFValue(18),
+                                                fontWeight: 'bold',
+                                                fill: '#fff'
+                                            }
+                                        }}
+                                    />
+                                </ChartContainer>
+
+                            )}
+                            {totalByCategories.map(item => (
+                                <HistoryCard
+                                    key={item.key}
+                                    title={item.name}
+                                    amount={item.totalFormatted}
+                                    color={item.color}
+                                />
+                            ))
                             }
                         </>
                 }
